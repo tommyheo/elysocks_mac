d8442ecf9e60d417101736c980062993802e9f3c
tommyheo <jyhuh8775@naver.com>
2024-08-25 06:11:12 +0900

    reset

--- backend/routers/adminRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const adminRouter = require('../controllers/adminController');
-
-// ----------------- Routers -----------------
-router.post('/users', adminRouter.users);
-
-module.exports = router;
